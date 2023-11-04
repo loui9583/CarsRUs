f45c588246355154a4c7b8af415beaf4774dcedd
louis <loui9583@stud.kea.dk>
2023-11-04 15:17:50 +0100

    changed from H2 to real db

---
+++ src/main/java/dat3/cars/entity/TestEntity.java
@@ -0,0 +1,24 @@
+package dat3.cars.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class TestEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String name;
+
+
+}
